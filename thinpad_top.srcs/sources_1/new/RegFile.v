cd0223ec4e91bb555f10a1a0003db71be47182b1
Xiangze-Li <lee_johnson@qq.com>
2020-11-07 21:24:33 +0800

    更新RegFile

--- thinpad_top.srcs/sources_1/new/RegFile.v
+++ thinpad_top.srcs/sources_1/new/RegFile.v
@@ -1,21 +1,23 @@
 `timescale 1ns / 1ps
 `default_nettype none
 
-module RegFile(input wire clk,
-               input wire rst,
-               input wire regWr,          // 1 for write enable
-               input wire[4:0] rs1,
-               rs2,
-               rd,
-               input wire[31:0] inData,
-               output wire[31:0] rs1Data,
-               rs2Data);
-    
-    reg[31:0] registers[31:0];
-    
+module RegFile(
+    input  wire         clk,
+    input  wire         rst,
+    input  wire         regWr,          // 1 for write enable
+    input  wire [4:0]   rs1,
+                        rs2,
+                        rd,
+    input  wire [31:0]  inData,
+    output wire [31:0]  rs1Data,
+                        rs2Data
+);
+
+    reg [31:0] registers[31:0];
+
     assign rs1Data = registers[rs1];
     assign rs2Data = registers[rs2];
-    
+
     integer i;
     always @(posedge clk or posedge rst) begin
         if (rst) begin
@@ -25,9 +27,9 @@ module RegFile(input wire clk,
         end
         else begin
             if (regWr && (rd !== 5'b0)) begin
-                registers[rd] <= data;
+                registers[rd] <= inData;
             end
         end
     end
-    
+
 endmodule
