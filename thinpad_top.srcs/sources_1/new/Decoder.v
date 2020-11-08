89cf6a6513569840bbc0bb8c7061ada2fc85834d
lvst <lvst17@mails.tsinghua.edu.cn>
2020-11-08 21:22:25 +0800

    Decoder初步完成，JAL指令经过EXE之后次态修改为了WB

---
+++ thinpad_top.srcs/sources_1/new/Decoder.v
@@ -0,0 +1,454 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/11/08 14:33:20
+// Design Name: 
+// Module Name: Decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Decoder(
+    input wire [31:0]   inst,
+    input wire          flagZ,
+    input wire [2:0]    stage,
+
+    output reg          pcWr,
+    output reg          pcNowWr,
+    output reg          pcSel,
+    output reg          ramSel,
+    output reg          ramWr,
+    output reg          ramRd,
+    output reg[1:0]    ramByte,
+    output reg          irWr,
+    output reg [1:0]    regDSel,
+    output reg [2:0]    immSel,
+    output reg          regWr,
+    output reg [1:0]    aluASel,
+    output reg [1:0]    aluBSel,
+    output wire [2:0]    func3,
+    output wire [6:0]    func7,
+
+    output reg [2:0]    stageNext
+    );
+
+    parameter [2:0]
+    // Stages
+        IDLE = 3'b000,
+        IF = 3'b001,
+        ID  = 3'b010,
+        EXE = 3'b011,
+        MEM = 3'b100,
+        WB  = 3'b101,
+        ERR = 3'b111;
+    
+    parameter [6:0]
+    // OpCode
+        OP_R        = 7'b0110011,
+        OP_I        = 7'b0010011,
+        OP_S        = 7'b0100011,
+        OP_L        = 7'b0000011,
+        OP_B        = 7'b1100011,
+        // opcodes below not used in exp-5
+        //OP_LOAD     = 7'b0000011,
+        OP_JALR     = 7'b1100111,
+        OP_JAL      = 7'b1101111,
+        OP_AUIPC    = 7'b0010111,
+        OP_LUI      = 7'b0110111;
+
+    assign func3 = inst[14:12];
+    assign func7 = inst[31:25];
+
+    wire [6:0]  opCode;
+    assign opCode = inst[6:0];
+
+    parameter [2:0]
+        IMM_N = 3'b000,
+        // immSel
+        IMM_I = 3'b001,
+        IMM_S = 3'b010,
+        IMM_B = 3'b011,
+        IMM_U = 3'b100,
+        IMM_J = 3'b101;
+
+    always @(*) begin
+        case (stage)
+        // 每个阶段为当前阶段准备控制信号!
+            IF : begin
+                pcWr        <= 1'b0;
+                pcNowWr     <= 1'b0;
+                pcSel       <= 1'b1;
+                ramSel      <= 1'b0;
+                ramWr       <= 1'b0;
+                ramRd       <= 1'b1;
+                ramByte     <= func3[1:0];
+                irWr        <= 1'b1;
+                regDSel     <= 2'b11;
+                immSel      <= IMM_N;
+                regWr       <= 1'b0;
+                aluASel     <= 2'b00;
+                aluBSel     <= 2'b00;
+            end
+            ID : begin
+                pcWr        <= 1'b0;
+                pcNowWr     <= 1'b0;
+                pcSel       <= 1'b1;
+                ramSel      <= 1'b0;
+                ramWr       <= 1'b0;
+                ramRd       <= 1'b0;
+                ramByte     <= func3[1:0];
+                irWr        <= 1'b0;
+                regDSel     <= 2'b11;
+                immSel      <= IMM_B;
+                regWr       <= 1'b0;
+                aluASel     <= 2'b01;
+                aluBSel     <= 2'b10;
+            end
+            EXE : begin
+                case (opCode)
+                    OP_R : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_N;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b10;
+                        aluBSel     <= 2'b01;
+                    end
+                    OP_I : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_I;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b10;
+                        aluBSel     <= 2'b10;
+                    end
+                    OP_L : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_I;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b10;
+                        aluBSel     <= 2'b10;
+                    end
+                    OP_S : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_S;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b10;
+                        aluBSel     <= 2'b10;
+                    end
+                    OP_B : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b0;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_N;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b10;
+                        aluBSel     <= 2'b01;
+                    end
+                    OP_JAL : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b0;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_J;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b00; //PC
+                        aluBSel     <= 2'b10; //imm
+                    end
+                    OP_JALR : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b0;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_I;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b10; //rs1
+                        aluBSel     <= 2'b10; //imm
+                    end
+                    OP_LUI : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b0;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_U;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b11; //不选择
+                        aluBSel     <= 2'b10; //imm
+                    end
+                    OP_AUIPC : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b0;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_U;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b11; //不选择
+                        aluBSel     <= 2'b10; //imm
+                    end
+                    default : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'bX;
+                        ramSel      <= 1'bX;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'bXX;
+                        immSel      <= 3'bXXX;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'bXX;
+                        aluBSel     <= 2'bXX;
+                    end
+                endcase
+            end
+            MEM : begin
+                case (opCode)
+                    OP_S : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b1;
+                        ramWr       <= 1'b1;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_S;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b10;
+                        aluBSel     <= 2'b10;
+                    end
+                    OP_L : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b1;
+                        ramWr       <= 1'b1;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_I;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b10;
+                        aluBSel     <= 2'b10;
+                    end
+                    default : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_N;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'b11;
+                        aluBSel     <= 2'b11;
+                    end
+                endcase
+            end
+            WB : begin
+                case (opCode)
+                    OP_I, OP_R : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b01; //写回rd
+                        immSel      <= IMM_N;
+                        regWr       <= 1'b1;
+                        aluASel     <= 2'b11;
+                        aluBSel     <= 2'b11;
+                    end
+                    OP_L : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b00; //写回ram中的数据
+                        immSel      <= IMM_N;
+                        regWr       <= 1'b1;
+                        aluASel     <= 2'b11;
+                        aluBSel     <= 2'b11;
+                    end
+                    OP_JALR, OP_JAL : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b10; //写回pc+4
+                        immSel      <= IMM_N;
+                        regWr       <= 1'b1;
+                        aluASel     <= 2'b11;
+                        aluBSel     <= 2'b11;
+                    end
+                    OP_LUI, OP_AUIPC : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b10; //写回rd
+                        immSel      <= IMM_N;
+                        regWr       <= 1'b1;
+                        aluASel     <= 2'b11;
+                        aluBSel     <= 2'b11;
+                    end
+                    default : begin
+                        pcWr        <= 1'b0;
+                        pcNowWr     <= 1'b0;
+                        pcSel       <= 1'b1;
+                        ramSel      <= 1'b0;
+                        ramWr       <= 1'b0;
+                        ramRd       <= 1'b0;
+                        ramByte     <= func3[1:0];
+                        irWr        <= 1'b0;
+                        regDSel     <= 2'b11;
+                        immSel      <= IMM_N;
+                        regWr       <= 1'b0;
+                        aluASel     <= 2'bXX;
+                        aluBSel     <= 2'bXX;
+                    end
+                endcase
+            end
+            default : begin
+                pcWr        <= 1'b0;
+                pcNowWr     <= 1'b0;
+                pcSel       <= 1'b1;
+                ramSel      <= 1'b0;
+                ramWr       <= 1'b0;
+                ramRd       <= 1'b0;
+                ramByte     <= 2'b11;
+                irWr        <= 1'b0;
+                regDSel     <= 2'b11;
+                immSel      <= IMM_N;
+                regWr       <= 1'b0;
+                aluASel     <= 2'bXX;
+                aluBSel     <= 2'bXX;
+            end
+        endcase
+    end
+
+    always @(*) begin
+    // Next Stage Gen.
+        case (stage)
+            IF1 :
+                stageNext = IF2;
+            IF2 :
+                stageNext = ID;
+            ID  :
+                stageNext = EXE;
+            EXE : begin
+                case (opCode)
+                    OP_R, OP_I  : stageNext = WB;
+                    OP_JAL      : stageNext = WB;
+                    OP_JALR     : stageNext = WB;
+                    OP_AUIPC    : stageNext = WB;
+                    OP_LUI      : stageNext = WB;
+                    OP_L, OP_S  : stageNext = ME;
+                    OP_B        : stageNext = IF;
+                    default     : stageNext = ERR;
+                endcase
+            end
+            ME : begin
+                case (opCode)
+                    OP_S : stageNext = IF;
+                    OP_L : stageNext = WB;
+                    default: stageNext = ERR;
+                endcase
+            end
+            WB :
+                stageNext = IF;
+            default:
+                stageNext = ERR;
+        endcase
+    end
+
+endmodule
--- thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -3,41 +3,41 @@
 module thinpad_top
 (
     input wire clk_50M,              //50MHz 时钟输入
-    input wire clk_11M0592,          //11.0592MHz 时钟输入（备用，可不用）
-    input wire clock_btn,            //BTN5手动时钟按钮开关，带消抖电路，按下时为1
-    input wire reset_btn,            //BTN6手动复位按钮开关，带消抖电路，按下时为1
-    input wire[3:0] touch_btn,       //BTN1~BTN4，按钮开关，按下时为1
-    input wire[31:0] dip_sw,         //32位拨码开关，拨到“ON”时为1
+    input wire clk_11M0592,          //11.0592MHz 时钟输入（�?�用，可不用�?
+    input wire clock_btn,            //BTN5手动时钟按钮开关，带消抖电�?，按下时�?1
+    input wire reset_btn,            //BTN6手动复位按钮开关，带消抖电�?，按下时�?1
+    input wire[3:0] touch_btn,       //BTN1~BTN4，按�?开关，按下时为1
+    input wire[31:0] dip_sw,         //32位拨码开关，拨到“ON”时�?1
     output wire[15:0] leds,          //16位LED，输出时1点亮
     output wire[7:0] dpy0,           //数码管低位信号，包括小数点，输出1点亮
     output wire[7:0] dpy1,           //数码管高位信号，包括小数点，输出1点亮
-    output wire uart_rdn,            //读串口信号，低有效
-    output wire uart_wrn,            //写串口信号，低有效
-    input wire uart_dataready,       //串口数据准备好
-    input wire uart_tbre,            //发送数据标志
-    input wire uart_tsre,            //数据发送完毕标志
-    inout wire[31:0] base_ram_data,  //BaseRAM数据，低8位与CPLD串口控制器共享
+    output wire uart_rdn,            //读串口信号，低有�?
+    output wire uart_wrn,            //写串口信号，低有�?
+    input wire uart_dataready,       //串口数据准�?�好
+    input wire uart_tbre,            //发送数�?标志
+    input wire uart_tsre,            //数据发送完毕标�?
+    inout wire[31:0] base_ram_data,  //BaseRAM数据，低8位与CPLD串口控制器共�?
     output wire[19:0] base_ram_addr, //BaseRAM地址
-    output wire[3:0] base_ram_be_n,  //BaseRAM字节使能，低有效。如果不使用字节使能，请保持为0
-    output wire base_ram_ce_n,       //BaseRAM片选，低有效
-    output wire base_ram_oe_n,       //BaseRAM读使能，低有效
-    output wire base_ram_we_n,       //BaseRAM写使能，低有效
+    output wire[3:0] base_ram_be_n,  //BaseRAM字节使能，低有效。�?�果不使用字节使能，请保持为0
+    output wire base_ram_ce_n,       //BaseRAM片选，低有�?
+    output wire base_ram_oe_n,       //BaseRAM读使能，低有�?
+    output wire base_ram_we_n,       //BaseRAM写使能，低有�?
     inout wire[31:0] ext_ram_data,   //ExtRAM数据
     output wire[19:0] ext_ram_addr,  //ExtRAM地址
-    output wire[3:0] ext_ram_be_n,   //ExtRAM字节使能，低有效。如果不使用字节使能，请保持为0
-    output wire ext_ram_ce_n,        //ExtRAM片选，低有效
-    output wire ext_ram_oe_n,        //ExtRAM读使能，低有效
-    output wire ext_ram_we_n,        //ExtRAM写使能，低有效
-    output wire txd,                 //直连串口发送端
-    input wire rxd,                  //直连串口接收端
-    output wire [22:0]flash_a,       //Flash地址，a0仅在8bit模式有效，16bit模式无意义
+    output wire[3:0] ext_ram_be_n,   //ExtRAM字节使能，低有效。�?�果不使用字节使能，请保持为0
+    output wire ext_ram_ce_n,        //ExtRAM片选，低有�?
+    output wire ext_ram_oe_n,        //ExtRAM读使能，低有�?
+    output wire ext_ram_we_n,        //ExtRAM写使能，低有�?
+    output wire txd,                 //直连串口发送�??
+    input wire rxd,                  //直连串口接收�?
+    output wire [22:0]flash_a,       //Flash地址，a0仅在8bit模式有效�?16bit模式无意�?
     inout wire [15:0]flash_d,        //Flash数据
     output wire flash_rp_n,          //Flash复位信号，低有效
-    output wire flash_vpen,          //Flash写保护信号，低电平时不能擦除、烧写
-    output wire flash_ce_n,          //Flash片选信号，低有效
-    output wire flash_oe_n,          //Flash读使能信号，低有效
-    output wire flash_we_n,          //Flash写使能信号，低有效
-    output wire flash_byte_n,        //Flash 8bit模式选择，低有效。在使用flash的16位模式时请设为1
+    output wire flash_vpen,          //Flash写保护信号，低电平时不能擦除、烧�?
+    output wire flash_ce_n,          //Flash片选信号，低有�?
+    output wire flash_oe_n,          //Flash读使能信号，低有�?
+    output wire flash_we_n,          //Flash写使能信号，低有�?
+    output wire flash_byte_n,        //Flash 8bit模式选择，低有效。在使用flash�?16位模式时请�?�为1
     output wire sl811_a0,
     output wire sl811_wr_n,
     output wire sl811_rd_n,
@@ -53,14 +53,14 @@ module thinpad_top
     output wire dm9k_cs_n,
     output wire dm9k_pwrst_n,
     input wire dm9k_int,
-    output wire[2:0] video_red,      //红色像素，3位
-    output wire[2:0] video_green,    //绿色像素，3位
-    output wire[1:0] video_blue,     //蓝色像素，2位
-    output wire video_hsync,         //行同步（水平同步）信号
-    output wire video_vsync,         //场同步（垂直同步）信号
+    output wire[2:0] video_red,      //红色像素�?3�?
+    output wire[2:0] video_green,    //绿色像素�?3�?
+    output wire[1:0] video_blue,     //蓝色像素�?2�?
+    output wire video_hsync,         //行同步（水平同�?�）信号
+    output wire video_vsync,         //场同步（垂直同�?�）信号
     output wire video_clk,           //像素时钟输出
     output wire video_de
-);           //行数据有效信号，用于区分消隐区
+);           //行数�?有效信号，用于区分消隐区
 
     wire clk, rst;
     assign clk = clk_50M;
@@ -180,8 +180,8 @@ module thinpad_top
     );
 
     ALU alu(
-        .func3(aluFunc3),
-        .func7(aluFunc7),
+        .funct3(aluFunc3),
+        .funct7(aluFunc7),
         .oprandA(oprandA),
         .oprandB(oprandB),
 
@@ -209,7 +209,7 @@ module thinpad_top
         .baseOeN(base_ram_oe_n),
         .baseWeN(base_ram_we_n),
 
-        .extIO(ext_ram_addr),
+        .extIO(ext_ram_data),
         .extAddr(ext_ram_addr),
         .extCeN(ext_ram_ce_n),
         .extBeN(ext_ram_be_n),
@@ -220,7 +220,7 @@ module thinpad_top
         .uartTbrE(uart_tbre),
         .uartTsrE(uart_tsre),
         .uartRdN(uart_rdn),
-        .uartWrn(uart_wrn)
+        .uartWrN(uart_wrn)
     );
 
     always @(posedge clk or posedge rst) begin
@@ -259,23 +259,23 @@ module thinpad_top
     end
     /* ==  ==  ==  ==  ==  = Demo code begin ==  ==  ==  ==  ==  = */
 
-    // // PLL分频示例
+    // // PLL分�?�示�?
     // wire locked, clk_10M, clk_20M;
     // pll_example clock_gen
     // (
     // // Clock in ports
     // .clk_in1(clk_50M),  // 外部时钟输入
     // // Clock out ports
-    // .clk_out1(clk_10M), // 时钟输出1，频率在IP配置界面中设置
-    // .clk_out2(clk_20M), // 时钟输出2，频率在IP配置界面中设置
+    // .clk_out1(clk_10M), // 时钟输出1，�?�率在IP配置界面�?设置
+    // .clk_out2(clk_20M), // 时钟输出2，�?�率在IP配置界面�?设置
     // // Status and control signals
     // .reset(reset_btn), // PLL复位输入
-    // .locked(locked)    // PLL锁定指示输出，"1"表示时钟稳定，
-    // // 后级电路复位信号应当由它生成（见下）
+    // .locked(locked)    // PLL锁定指示输出�?"1"表示时钟稳定�?
+    // // 后级电路复位信号应当由它生成（�?�下�?
     // );
 
     // reg reset_of_clk10M;
-    // // 异步复位，同步释放，将locked信号转为后级电路的复位reset_of_clk10M
+    // // 异�?��?�位，同步释放，将locked信号�?为后级电�?的�?�位reset_of_clk10M
     // always@(posedge clk_10M or negedge locked) begin
     //     if (~locked) reset_of_clk10M <= 1'b1;
     //     else        reset_of_clk10M  <= 1'b0;
